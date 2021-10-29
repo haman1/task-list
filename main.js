0a854d355bafa4dbeed1bfc5364d9973a42baacd
ombasa <ombasaherman@gmail.com>
2021-10-30 00:12:32 +0300

    functionality

---
+++ main.js
@@ -0,0 +1,17 @@
+//Define ui variables
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+//load  event listners
+
+loadEventListeners();
+
+//function to load all
+function loadEventListeners() {
+    
+}
