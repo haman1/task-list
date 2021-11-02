912555608d89cba4fc3aa2ad15ca5de2e4f2c3cc
ombasa <ombasaherman@gmail.com>
2021-11-03 00:38:24 +0300

    add task

--- main.js
+++ main.js
@@ -18,3 +18,40 @@ function loadEventListeners() {
     
 }
 
+
+//Add Task 
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add Task');
+    }
+
+    //create li elements
+
+    const li = document.createElement('li');
+    //add class
+    li.className= 'collection-item';
+    //create textnode and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // link element
+    const link = document.createElement('a');
+
+    //add class
+    link.className = 'delete-item secondary-content';
+
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></li>';
+
+    //append li to ul
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+
+    //clear input 
+    taskInput.value = '';
+
+
+
+    e.preventDefault();
+}
