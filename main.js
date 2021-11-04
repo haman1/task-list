d7ba40bbb92f30bce3a0ea1f68d53630e2ba9357
ombasa <ombasaherman@gmail.com>
2021-11-04 20:16:21 +0300

    filter tasks

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ function loadEventListeners() {
     //clear task event
     clearBtn.addEventListener('click', clearTasks);
 
+    //Filter tasks event
+
+    filter.addEventListener('keyup', filterTasks);
+
 
     
 }
@@ -85,4 +89,20 @@ function clearTasks() {
    while(taskList.firstChild) {
        taskList.removeChild(taskList.firstChild);
    }
+}
+
+/// filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+    (function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
